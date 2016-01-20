c6a6c6e274ccc1140c60640a5e2ca11b0a504baf
Fong Chun Chan <fongchun@alumni.ubc.ca>
2016-01-20 14:12:56 -0800

    Fixing Bug
    * Prepending :: to functions that are associated with imported packages.
    * Adding Roxygen comments for parameters.
    * Change to using " instead of '

--- R/timesweep.R
+++ R/timesweep.R
@@ -4,12 +4,16 @@
 #'
 #' @import htmlwidgets, jsonlite
 #'
+#' @param patient Patient name.
+#' @param clonal_prev_csv.
+#' @param tree_gml Path to GML file.
 #' @export
-timesweep <- function(patient, clonal_prev_csv, tree_gml, width = NULL, height = NULL) {
+timesweep <- function(patient, clonal_prev_csv, tree_gml, width = NULL, 
+                      height = NULL) {
 
   # parse csv
   clonal_prev_data = read.csv(clonal_prev_csv)
-  clonal_prev_JSON <- toJSON(clonal_prev_data)
+  clonal_prev_JSON <- jsonlite::toJSON(clonal_prev_data)
   gmlString <- paste(readLines(tree_gml), collapse=" ")
 
   # forward options using x
@@ -21,19 +25,20 @@ timesweep <- function(patient, clonal_prev_csv, tree_gml, width = NULL, height =
 
   # create widget
   htmlwidgets::createWidget(
-    name = 'timesweep',
+    name = "timesweep",
     x,
     width = width,
     height = height,
-    package = 'timesweep'
+    package = "timesweep"
   )
 }
 
 #' Widget output function for use in Shiny
 #'
 #' @export
-timesweepOutput <- function(outputId, width = '100%', height = '400px'){
-  shinyWidgetOutput(outputId, 'timesweep', width, height, package = 'timesweep')
+timesweepOutput <- function(outputId, width = "100%", height = "400px"){
+  htmlwidgets::shinyWidgetOutput(outputId, "timesweep", width, height, 
+                                 package = "timesweep")
 }
 
 #' Widget render function for use in Shiny
@@ -41,5 +46,5 @@ timesweepOutput <- function(outputId, width = '100%', height = '400px'){
 #' @export
 renderTimesweep <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
-  shinyRenderWidget(expr, timesweepOutput, env, quoted = TRUE)
+  htmlwidgets::shinyRenderWidget(expr, timesweepOutput, env, quoted = TRUE)
 }
