050157e73e80ff2469b462e2a18e9d011c4a40da
Maia Smith <msmith@bccrc.ca>
2016-04-13 13:02:30 -0700

    svg download bug fixed
    After downloading svg, interactivity with d3 didn't work. Now that's fixed.

--- inst/htmlwidgets/lib/my_svg_crowbar.js
+++ inst/htmlwidgets/lib/my_svg_crowbar.js
@@ -1,31 +1,24 @@
-// Downloads the first SVG element on the page
+// Downloads the SVGs on the webpage
 function downloadSVG() {
-  var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
 
   window.URL = (window.URL || window.webkitURL);
 
-  var body = document.body,
-      emptySvg;
+  var body = document.body;
 
   var prefix = {
     xmlns: "http://www.w3.org/2000/xmlns/",
     xlink: "http://www.w3.org/1999/xlink",
     svg: "http://www.w3.org/2000/svg"
-  };
+  }
 
   initialize();
 
   function initialize() {
     var documents = [window.document],
-        newSource,
+        SVGSources = [];
         iframes = document.querySelectorAll("iframe"),
         objects = document.querySelectorAll("object");
-
-    // get the CSS of the svg by creating an empty svg, retrieving its style, then deleting it
-    var emptySvg = window.document.createElementNS(prefix.svg, 'svg');
-    window.document.body.appendChild(emptySvg); 
-    var emptySvgDeclarationComputed = getComputedStyle(emptySvg);
-    window.document.body.removeChild(emptySvg); 
 
     [].forEach.call(iframes, function(el) {
       try {
@@ -33,7 +26,7 @@ function downloadSVG() {
           documents.push(el.contentDocument);
         }
       } catch(err) {
-        console.log(err);
+        console.log(err)
       }
     });
 
@@ -48,50 +41,147 @@ function downloadSVG() {
     });
 
     documents.forEach(function(doc) {
-      newSource = getSource(doc, emptySvgDeclarationComputed);
-    });
-    if (newSource) {
-      download(newSource);
+      var styles = getStyles(doc);
+      var newSources = getSources(doc, styles);
+      // because of prototype on NYT pages
+      for (var i = 0; i < newSources.length; i++) {
+        SVGSources.push(newSources[i]);
+      };
+    })
+    if (SVGSources.length > 1) {
+      createPopover(SVGSources);
+    } else if (SVGSources.length > 0) {
+      download(SVGSources[0]);
     } else {
-      alert("Couldn’t find any SVG nodes.");
+      alert("The Crowbar couldn’t find any SVG nodes.");
     }
+  }
+
+  function createPopover(sources) {
+    cleanup();
+
+    sources.forEach(function(s1) {
+      sources.forEach(function(s2) {
+        if (s1 !== s2) {
+          if ((Math.abs(s1.top - s2.top) < 38) && (Math.abs(s1.left - s2.left) < 38)) {
+            s2.top += 38;
+            s2.left += 38;
+          }
+        }
+      })
+    });
+
+    var buttonsContainer = document.createElement("div");
+    body.appendChild(buttonsContainer);
+
+    buttonsContainer.setAttribute("class", "svg-crowbar");
+    buttonsContainer.style["z-index"] = 1e7;
+    buttonsContainer.style["position"] = "absolute";
+    buttonsContainer.style["top"] = 0;
+    buttonsContainer.style["left"] = 0;
+
+
+
+    var background = document.createElement("div");
+    body.appendChild(background);
+
+    background.setAttribute("class", "svg-crowbar");
+    background.style["background"] = "rgba(255, 255, 255, 0.7)";
+    background.style["position"] = "fixed";
+    background.style["left"] = 0;
+    background.style["top"] = 0;
+    background.style["width"] = "100%";
+    background.style["height"] = "100%";
+
+    sources.forEach(function(d, i) {
+      var buttonWrapper = document.createElement("div");
+      buttonsContainer.appendChild(buttonWrapper);
+      buttonWrapper.setAttribute("class", "svg-crowbar");
+      buttonWrapper.style["position"] = "absolute";
+      buttonWrapper.style["top"] = (d.top + document.body.scrollTop) + "px";
+      buttonWrapper.style["left"] = (document.body.scrollLeft + d.left) + "px";
+      buttonWrapper.style["padding"] = "4px";
+      buttonWrapper.style["border-radius"] = "3px";
+      buttonWrapper.style["color"] = "white";
+      buttonWrapper.style["text-align"] = "center";
+      buttonWrapper.style["font-family"] = "'Helvetica Neue'";
+      buttonWrapper.style["background"] = "rgba(0, 0, 0, 0.8)";
+      buttonWrapper.style["box-shadow"] = "0px 4px 18px rgba(0, 0, 0, 0.4)";
+      buttonWrapper.style["cursor"] = "move";
+      buttonWrapper.textContent =  "SVG #" + i + ": " + (d.id ? "#" + d.id : "") + (d.class ? "." + d.class : "");
+
+      var button = document.createElement("button");
+      buttonWrapper.appendChild(button);
+      button.setAttribute("data-source-id", i)
+      button.style["width"] = "150px";
+      button.style["font-size"] = "12px";
+      button.style["line-height"] = "1.4em";
+      button.style["margin"] = "5px 0 0 0";
+      button.textContent = "Download";
+
+      button.onclick = function(el) {
+        // console.log(el, d, i, sources)
+        download(d);
+      };
+
+    });
 
   }
 
-  function getSource(doc, emptySvgDeclarationComputed) {
-    var svgInfo,
-        svg = doc.querySelector("svg"); // selects the first svg on the page
+  function cleanup() {
+    var crowbarElements = document.querySelectorAll(".svg-crowbar");
 
-    svg.setAttribute("version", "1.1");
+    [].forEach.call(crowbarElements, function(el) {
+      el.parentNode.removeChild(el);
+    });
+  }
 
-    // removing attributes so they aren't doubled up
-    svg.removeAttribute("xmlns");
-    svg.removeAttribute("xlink");
 
-    // These are needed for the svg
-    if (!svg.hasAttributeNS(prefix.xmlns, "xmlns")) {
-      svg.setAttributeNS(prefix.xmlns, "xmlns", prefix.svg);
-    }
+  function getSources(doc, styles) {
+    var svgInfo = [],
+        svgs = doc.querySelectorAll("svg");
 
-    if (!svg.hasAttributeNS(prefix.xmlns, "xmlns:xlink")) {
-      svg.setAttributeNS(prefix.xmlns, "xmlns:xlink", prefix.xlink);
-    }
+    styles = (styles === undefined) ? "" : styles;
 
-    setInlineStyles(svg, emptySvgDeclarationComputed);
+    [].forEach.call(svgs, function (svg) {
 
-    var source = (new XMLSerializer()).serializeToString(svg);
-    var rect = svg.getBoundingClientRect();
-    svgInfo = {
-      top: rect.top,
-      left: rect.left,
-      width: rect.width,
-      height: rect.height,
-      class: svg.getAttribute("class"),
-      id: svg.getAttribute("id"),
-      childElementCount: svg.childElementCount,
-      source: [doctype + source]
-    };
+      svg.setAttribute("version", "1.1");
 
+      var defsEl = document.createElement("defs");
+      svg.insertBefore(defsEl, svg.firstChild); //TODO   .insert("defs", ":first-child")
+      // defsEl.setAttribute("class", "svg-crowbar");
+
+      var styleEl = document.createElement("style")
+      defsEl.appendChild(styleEl);
+      styleEl.setAttribute("type", "text/css");
+
+
+      // removing attributes so they aren't doubled up
+      svg.removeAttribute("xmlns");
+      svg.removeAttribute("xlink");
+
+      // These are needed for the svg
+      if (!svg.hasAttributeNS(prefix.xmlns, "xmlns")) {
+        svg.setAttributeNS(prefix.xmlns, "xmlns", prefix.svg);
+      }
+
+      if (!svg.hasAttributeNS(prefix.xmlns, "xmlns:xlink")) {
+        svg.setAttributeNS(prefix.xmlns, "xmlns:xlink", prefix.xlink);
+      }
+
+      var source = (new XMLSerializer()).serializeToString(svg).replace('</style>', '<![CDATA[' + styles + ']]></style>');
+      var rect = svg.getBoundingClientRect();
+      svgInfo.push({
+        top: rect.top,
+        left: rect.left,
+        width: rect.width,
+        height: rect.height,
+        class: svg.getAttribute("class"),
+        id: svg.getAttribute("id"),
+        childElementCount: svg.childElementCount,
+        source: [doctype + source]
+      });
+    });
     return svgInfo;
   }
 
@@ -121,45 +211,34 @@ function downloadSVG() {
     }, 10);
   }
 
+  function getStyles(doc) {
+    var styles = "",
+        styleSheets = doc.styleSheets;
 
-  function setInlineStyles(svg, emptySvgDeclarationComputed) {
-
-    function explicitlySetStyle (element) {
-      var cSSStyleDeclarationComputed = getComputedStyle(element);
-      var i, len, key, value;
-      var computedStyleStr = "";
-      for (i=0, len=cSSStyleDeclarationComputed.length; i<len; i++) {
-        key=cSSStyleDeclarationComputed[i];
-        value=cSSStyleDeclarationComputed.getPropertyValue(key);
-        if (value!==emptySvgDeclarationComputed.getPropertyValue(key)) {
-          computedStyleStr+=key+":"+value+";";
-        }
+    if (styleSheets) {
+      for (var i = 0; i < styleSheets.length; i++) {
+        processStyleSheet(styleSheets[i]);
       }
-      element.setAttribute('style', computedStyleStr);
     }
-    function traverse(obj){
-      var tree = [];
-      tree.push(obj);
-      visit(obj);
-      function visit(node) {
-        if (node && node.hasChildNodes()) {
-          var child = node.firstChild;
-          while (child) {
-            if (child.nodeType === 1 && child.nodeName != 'SCRIPT'){
-              tree.push(child);
-              visit(child);
+
+    function processStyleSheet(ss) {
+      if (ss.cssRules) {
+        for (var i = 0; i < ss.cssRules.length; i++) {
+          var rule = ss.cssRules[i];
+          if (rule.type === 3) {
+            // Import Rule
+            processStyleSheet(rule.styleSheet);
+          } else {
+            // hack for illustrator crashing on descendent selectors
+            if (rule.selectorText) {
+              if (rule.selectorText.indexOf(">") === -1) {
+                styles += "\n" + rule.cssText;
+              }
             }
-            child = child.nextSibling;
           }
         }
       }
-      return tree;
     }
-    // hardcode computed css styles inside svg
-    var allElements = traverse(svg);
-    var i = allElements.length;
-    while (i--){
-      explicitlySetStyle(allElements[i]);
-    }
+    return styles;
   }
 }
