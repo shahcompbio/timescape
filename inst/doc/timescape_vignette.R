3dd2922c913b095a897cff67bec94c55f297262d
Maia Smith <msmith@momac27.bccrc.ca>
2016-10-13 16:32:59 -0700

    updated readme with live files

---
+++ inst/doc/timescape_vignette.R
@@ -0,0 +1,10 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("devtools") # if not already installed
+#  library(devtools)
+#  install_bitbucket("MO_BCCRC/timescape")
+#  library(timescape)
+#  example(timescape) # to run example
+
+## ---- eval=FALSE---------------------------------------------------------
+#  ?timescape
+
